a9770b7b19c36763c38b10ab5ba62223bf9b941e
Hori <sougo08110811@gmail.com>
2024-06-24 10:55:30 +0900

    Initial Commit

---
+++ AR_SwiftUI/AR_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_SwiftUIApp.swift
+//  AR_SwiftUI
+//
+//  Created by 堀壮吾 on 2024/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AR_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
